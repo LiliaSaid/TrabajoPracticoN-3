0a68ceed532a5830a61f7821c5561194d794717d
LILI-PC <LILI-PC@DESKTOP-5ELCB6R>
2018-04-25 23:29:30 -0300

    inicializando proyecto tp4

---
+++ src/java/pto1/aplicacion/dominio/TemperaturaCentigrado.java
@@ -0,0 +1,15 @@
+
+package pto1.aplicacion.dominio;
+
+public class TemperaturaCentigrado {
+    private int grado;
+    double Fahrenheit;
+    
+    public int getGrado() {
+        return grado;
+    }
+
+    public void setGrado(int grado) {
+        this.grado = grado;
+    }
+}
